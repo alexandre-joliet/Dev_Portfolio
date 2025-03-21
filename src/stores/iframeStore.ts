70ed3edb0aa34789e4bd88549151e46944eb3868
alexandre-joliet <alexandre.joliet@gmail.com>
2025-03-21 16:16:21 +0100

    :card_file_box: Update store with new states

--- src/stores/iframeStore.ts
+++ src/stores/iframeStore.ts
@@ -5,13 +5,42 @@ type IframeState = {
     updateShowIntro: (value: boolean) => void;
     showContent: boolean;
     updateShowContent: (value: boolean) => void;
+    showAbout: boolean;
+    showActivities: boolean;
+    showSkills: boolean;
+    showProjects: boolean;
+    updateShowPage: (
+        page: "Intro" | "About" | "Activities" | "Skills" | "Projects"
+    ) => void;
+    showEnterAnimation: boolean;
+    showExitAnimation: boolean;
+    updateShowAnimation: (value: "Enter" | "Exit") => void;
 };
 
 const useIframeStore = create<IframeState>((set) => ({
-    showIntro: true,
-    updateShowIntro: (value) => set({ showIntro: value }),
     showContent: false,
     updateShowContent: (value) => set({ showContent: value }),
+    showIntro: true,
+    updateShowIntro: (value) => set({ showIntro: value }),
+    showAbout: false,
+    showActivities: false,
+    showSkills: false,
+    showProjects: false,
+    updateShowPage: (page) =>
+        set({
+            showIntro: page === "Intro",
+            showAbout: page === "About",
+            showActivities: page === "Activities",
+            showSkills: page === "Skills",
+            showProjects: page === "Projects",
+        }),
+    showEnterAnimation: false,
+    showExitAnimation: false,
+    updateShowAnimation: (value) =>
+        set({
+            showEnterAnimation: value === "Enter",
+            showExitAnimation: value === "Exit",
+        }),
 }));
 
 export default useIframeStore;
