5e980b2b9752b9819cc834c07cb6952a7616e602
alexandre-joliet <alexandre.joliet@gmail.com>
2025-03-21 16:57:34 +0100

    :construction: WIP - potential update - does not work yet

--- src/pages/desktop/desktop_components/DesktopCanvas/DesktopCanvas.tsx
+++ src/pages/desktop/desktop_components/DesktopCanvas/DesktopCanvas.tsx
@@ -1,8 +1,8 @@
 import "./DesktopCanvas.css";
 import { Canvas } from "@react-three/fiber";
 import * as THREE from "three";
-import { OrbitControls, Points, useGLTF } from "@react-three/drei";
-import { useEffect, useMemo, useRef } from "react";
+import { OrbitControls } from "@react-three/drei";
+import { useRef } from "react";
 import textMorphingVertexShader from "../../../../shaders/textMorphing/vertex.glsl";
 import textMorphingFragmentShader from "../../../../shaders/textMorphing/fragment.glsl";
 
@@ -21,62 +21,62 @@ const DesktopCanvas = () => {
     //     const positions = geometry.attributes.position.array;
     //     return new Float32Array(positions);
     // }, []);
-    const models = useGLTF("/models/textModels.glb");
+    // const models = useGLTF("/models/textModels.glb");
 
-    useEffect(() => {
-        const modelsPositions = models.scene.children.map((child) => {
-            return (child as THREE.Mesh).geometry.attributes.position;
-        });
+    // useEffect(() => {
+    //     const modelsPositions = models.scene.children.map((child) => {
+    //         return (child as THREE.Mesh).geometry.attributes.position;
+    //     });
 
-        // Harmonizing number of vertices
-        const particles: {
-            maxCount?: number;
-            positions?: THREE.Float32BufferAttribute[];
-        } = {};
-        particles.maxCount = 0;
-        for (const position of modelsPositions) {
-            if (position.count > particles.maxCount) {
-                particles.maxCount = position.count;
-            }
-        }
+    //     // Harmonizing number of vertices
+    //     const particles: {
+    //         maxCount?: number;
+    //         positions?: THREE.Float32BufferAttribute[];
+    //     } = {};
+    //     particles.maxCount = 0;
+    //     for (const position of modelsPositions) {
+    //         if (position.count > particles.maxCount) {
+    //             particles.maxCount = position.count;
+    //         }
+    //     }
 
-        // Process positions
-        particles.positions = [];
-        for (const position of modelsPositions) {
-            const originalArray = position.array;
-            const newArray = new Float32Array(particles.maxCount * 3); // x3 for XYZ axis
-            for (let i = 0; i < particles.maxCount; i++) {
-                const i3 = i * 3;
+    //     // Process positions
+    //     particles.positions = [];
+    //     for (const position of modelsPositions) {
+    //         const originalArray = position.array;
+    //         const newArray = new Float32Array(particles.maxCount * 3); // x3 for XYZ axis
+    //         for (let i = 0; i < particles.maxCount; i++) {
+    //             const i3 = i * 3;
 
-                if (i3 < originalArray.length) {
-                    newArray[i3 + 0] = originalArray[i3 + 0];
-                    newArray[i3 + 1] = originalArray[i3 + 1];
-                    newArray[i3 + 2] = originalArray[i3 + 2];
-                } else {
-                    newArray[i3 + 0] = 0;
-                    newArray[i3 + 1] = 0;
-                    newArray[i3 + 2] = 0;
-                }
-            }
+    //             if (i3 < originalArray.length) {
+    //                 newArray[i3 + 0] = originalArray[i3 + 0];
+    //                 newArray[i3 + 1] = originalArray[i3 + 1];
+    //                 newArray[i3 + 2] = originalArray[i3 + 2];
+    //             } else {
+    //                 newArray[i3 + 0] = 0;
+    //                 newArray[i3 + 1] = 0;
+    //                 newArray[i3 + 2] = 0;
+    //             }
+    //         }
 
-            // particles.positions?.push(
-            //     new THREE.Float32BufferAttribute(newArray, 3)
-            // );
+    //         // particles.positions?.push(
+    //         //     new THREE.Float32BufferAttribute(newArray, 3)
+    //         // );
 
-            return new THREE.Float32BufferAttribute(newArray, 3);
-        }
-    });
+    //         return new THREE.Float32BufferAttribute(newArray, 3);
+    //     }
+    // });
 
     // Getting models positions
 
-    const points = useMemo(() => {
-        // Create a sphere geometry and extract its vertices
-        const geometry = new THREE.SphereGeometry(1, 20, 20);
-        const positions = geometry.attributes.position.array;
-        console.log(positions);
+    // const points = useMemo(() => {
+    //     // Create a sphere geometry and extract its vertices
+    //     const geometry = new THREE.SphereGeometry(1, 20, 20);
+    //     const positions = geometry.attributes.position.array;
+    //     console.log(positions);
 
-        return new Float32Array(positions);
-    }, []);
+    //     return new Float32Array(positions);
+    // }, []);
 
     return (
         <>
@@ -85,7 +85,7 @@ const DesktopCanvas = () => {
             >
                 <OrbitControls></OrbitControls>
                 <points ref={geometryRef}>
-                    <primitive object={}></primitive>
+                    {/* <primitive object={}></primitive> */}
                     <shaderMaterial
                         vertexShader={textMorphingVertexShader}
                         fragmentShader={textMorphingFragmentShader}
