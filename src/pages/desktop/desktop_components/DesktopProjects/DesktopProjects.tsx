989769f3f2586de415a4a348b3ff72e901879486
alexandre-joliet <alexandre.joliet@gmail.com>
2025-03-27 14:46:03 +0100

    :construction: Improve styling but still WIP

--- src/pages/desktop/desktop_components/DesktopProjects/DesktopProjects.tsx
+++ src/pages/desktop/desktop_components/DesktopProjects/DesktopProjects.tsx
@@ -1,7 +1,7 @@
 import "./DesktopProjects.css";
 import gsap from "gsap";
-import useIframeStore from "../../../../stores/iframeStore";
 import { useEffect } from "react";
+import useIframeStore from "../../../../stores/iframeStore";
 
 const DesktopProjects = () => {
     //** Logic */
@@ -11,12 +11,12 @@ const DesktopProjects = () => {
     //** Style */
     useEffect(() => {
         if (showProjects) {
-            gsap.to(".desktop_portfolio-projects-section", {
+            gsap.to(".desktop_projects-section", {
                 duration: 2,
                 opacity: 1,
                 // filter: "blur(0px)",
             });
-            gsap.to(".desktop_portfolio-projects-section", {
+            gsap.to(".desktop_projects-section", {
                 duration: 1,
                 filter: "blur(0px)",
             });
@@ -25,7 +25,7 @@ const DesktopProjects = () => {
 
     useEffect(() => {
         if (showExitAnimation) {
-            gsap.to(".desktop_portfolio-projects-section", {
+            gsap.to(".desktop_projects-section", {
                 duration: 1,
                 opacity: 0,
             });
@@ -36,114 +36,165 @@ const DesktopProjects = () => {
     return (
         <>
             {showProjects && (
-                <section className="desktop_portfolio-projects-section">
-                    <h2 className="desktop_portfolio-projects-title">
-                        PROJECTS
-                    </h2>
-                    <div>
-                        <ul className="desktop_portfolio_projects-card-container">
-                            <li className="desktop_portfolio_projects-card">
+                <section className="desktop_projects-section">
+                    <h2 className="desktop_projects-title">PROJECTS</h2>
+                    <div className="desktop_projects-content-wrapper">
+                        <ul className="desktop_projects-cards-container">
+                            <li className="desktop_projects-card">
+                                <div className="desktop_projects-card-textcontent">
+                                    <p className="desktop_projects-card-title">
+                                        WEB PORTFOLIO
+                                    </p>
+                                    <div className="desktop_projects-card-info">
+                                        <p className="desktop_projects-card-info-title">
+                                            INFO
+                                        </p>
+                                        <p className="desktop_projects-card-info-text">
+                                            Built from scratch to showcase my
+                                            work
+                                        </p>
+                                        <p className="desktop_projects-card-info-title">
+                                            STACK
+                                        </p>
+                                        <p className="desktop_projects-card-info-text">
+                                            Blender, React, TypeScript,
+                                            React-router, Zustand, GSAP and Vite
+                                        </p>
+                                        <a
+                                            className="desktop_projects-card-link"
+                                            href="https://github.com/alexandre-joliet/Dev_Portfolio"
+                                            target="_blank"
+                                        >
+                                            <button className="desktop_projects-card-button">
+                                                <img
+                                                    className="desktop_projects-card-icons icon-github"
+                                                    src="/icons/github.png"
+                                                    alt=""
+                                                />
+                                                Repo
+                                            </button>
+                                        </a>
+                                    </div>
+                                </div>
                                 <img
                                     className="desktop_projects-card-image"
                                     src="/pictures/Portfolio.webp"
                                     alt=""
                                 ></img>
-                                <p className="desktop_projects-card-title">
-                                    WEB PORTFOLIO
-                                </p>
-                                <div className="desktop_projects-card-info">
-                                    <p className="desktop_projects-card-info-title">
-                                        INFO
-                                    </p>
-                                    <p className="projects-card-info-text">
-                                        Built from scratch to showcase my work
-                                    </p>
-                                    <p className="desktop_projects-card-info-title">
-                                        STACK
-                                    </p>
-                                    <p className="desktop_projects-card-info-text">
-                                        Blender, React, TypeScript,
-                                        React-router, Zustand, GSAP and Vite
-                                    </p>
-                                </div>
                             </li>
-                            <li className="desktop_portfolio_projects-card">
+                            <li className="desktop_projects-card">
+                                <div className="desktop_projects-card-textcontent">
+                                    <p className="desktop_projects-card-title">
+                                        PHOTOGRAPHY PORTFOLIO
+                                    </p>
+
+                                    <div className="desktop_projects-card-info">
+                                        <p className="desktop_projects-card-info-title">
+                                            INFO
+                                        </p>
+                                        <p className="desktop_projects-card-info-text">
+                                            Personal project to host and
+                                            showcase my photography work
+                                        </p>
+                                        <p className="desktop_projects-card-info-title">
+                                            STACK
+                                        </p>
+                                        <p className="desktop_projects-card-info-text">
+                                            NextJS, ReactJS, TypeScript, Framer
+                                            Motion and Cloudinary
+                                        </p>
+                                        <a
+                                            className="desktop_projects-card-link"
+                                            href="https://www.alexandrejoliet-photo.com/"
+                                            target="_blank"
+                                        >
+                                            <button className="desktop_projects-card-button">
+                                                <img
+                                                    className="desktop_projects-card-icons"
+                                                    src="/icons/up-arrow.png"
+                                                    alt=""
+                                                />
+                                                Live
+                                            </button>
+                                        </a>
+                                        <a
+                                            className="desktop_projects-card-link"
+                                            href="https://github.com/alexandre-joliet/Photo_Portfolio"
+                                            target="_blank"
+                                        >
+                                            <button className="desktop_projects-card-button">
+                                                <img
+                                                    className="desktop_projects-card-icons icon-github"
+                                                    src="/icons/github.png"
+                                                    alt=""
+                                                />
+                                                Repo
+                                            </button>
+                                        </a>
+                                    </div>
+                                </div>
                                 <img
                                     className="desktop_projects-card-image"
                                     src="/pictures/Photo.webp"
                                     alt=""
                                 ></img>
-                                <p className="desktop_projects-card-title">
-                                    PHOTOGRAPHY PORTFOLIO
-                                </p>
-                                <a
-                                    className="desktop_projects-card-link"
-                                    href="https://www.alexandrejoliet-photo.com/"
-                                    target="_blank"
-                                >
-                                    <button className="desktop_projects-card-button">
-                                        <img
-                                            src="/icons/link_wght200_48.png"
-                                            alt=""
-                                        />
-                                    </button>
-                                </a>
-                                <div className="desktop_projects-card-info">
-                                    <p className="desktop_projects-card-info-title">
-                                        INFO
-                                    </p>
-                                    <p className="desktop_projects-card-info-text">
-                                        Personal project to host and showcase my
-                                        photography work
-                                    </p>
-                                    <p className="desktop_projects-card-info-title">
-                                        STACK
-                                    </p>
-                                    <p className="desktop_projects-card-info-text">
-                                        NextJS, ReactJS, TypeScript, Framer
-                                        Motion and Cloudinary
-                                    </p>
-                                </div>
                             </li>
-                            <li className="desktop_portfolio_projects-card">
+                            <li className="desktop_projects-card">
+                                <div className="desktop_projects-card-textcontent">
+                                    <p className="desktop_projects-card-title">
+                                        VENDOME TAEKWONDO CLUB
+                                    </p>
+                                    <div className="desktop_projects-card-info">
+                                        <p className="desktop_projects-card-info-title">
+                                            INFO
+                                        </p>
+                                        <p className="desktop_projects-card-info-text">
+                                            Creation of a website for a local
+                                            sports association to increase its
+                                            visibility, share information and
+                                            recruit new members
+                                        </p>
+                                        <p className="desktop_projects-card-info-title">
+                                            STACK
+                                        </p>
+                                        <p className="desktop_projects-card-info-text">
+                                            NextJS, ReactJS and TypeScript
+                                        </p>
+                                        <a
+                                            className="desktop_projects-card-link"
+                                            href="https://www.vendome-taekwondo.com/"
+                                            target="_blank"
+                                        >
+                                            <button className="desktop_projects-card-button">
+                                                <img
+                                                    className="desktop_projects-card-icons"
+                                                    src="/icons/up-arrow.png"
+                                                    alt=""
+                                                />
+                                                Live
+                                            </button>
+                                        </a>
+                                        <a
+                                            className="desktop_projects-card-link"
+                                            href="https://github.com/alexandre-joliet/Vendome_TKD"
+                                            target="_blank"
+                                        >
+                                            <button className="desktop_projects-card-button">
+                                                <img
+                                                    className="desktop_projects-card-icons icon-github"
+                                                    src="/icons/github.png"
+                                                    alt=""
+                                                />
+                                                Repo
+                                            </button>
+                                        </a>
+                                    </div>
+                                </div>
                                 <img
                                     className="desktop_projects-card-image"
                                     src="/pictures/TKD.webp"
                                     alt=""
                                 ></img>
-                                <p className="desktop_projects-card-title">
-                                    VENDOME TAEKWONDO CLUB
-                                </p>
-
-                                <a
-                                    className="desktop_projects-card-link"
-                                    href="https://www.vendome-taekwondo.com/"
-                                    target="_blank"
-                                >
-                                    <button className="desktop_projects-card-button">
-                                        <img
-                                            src="/icons/link_wght200_48.png"
-                                            alt=""
-                                        />
-                                    </button>
-                                </a>
-                                <div className="desktop_projects-card-info">
-                                    <p className="desktop_projects-card-info-title">
-                                        INFO
-                                    </p>
-                                    <p className="desktop_projects-card-info-text">
-                                        Creation of a website for a local sports
-                                        association to increase its visibility,
-                                        share information and recruit new
-                                        members
-                                    </p>
-                                    <p className="desktop_projects-card-info-title">
-                                        STACK
-                                    </p>
-                                    <p className="desktop_projects-card-info-text">
-                                        NextJS, ReactJS and TypeScript
-                                    </p>
-                                </div>
                             </li>
                         </ul>
                     </div>
